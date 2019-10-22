a8f0492ea7033d9984a26a068a267b824bf8c26a
masnn <masnn0@outlook.com>
2019-10-22 18:09:02 +0800

    eslint & update install.js

--- install.js
+++ install.js
@@ -1,10 +1,13 @@
+/* eslint-disable */
 const
     fs = require('fs-extra'),
     path = require('path');
 
-console.info('Installing theme default');
-fs.ensureDirSync(path.resolve(__dirname, '..', '..', '.uibuild'));
-fs.copySync(path.resolve(__dirname, 'templates'), path.resolve(__dirname, '..', '..', '.uibuild', 'templates'));
-fs.copySync(path.resolve(__dirname, 'public'), path.resolve('..', '..', '.uibuild', 'public'));
-fs.copySync(path.resolve(__dirname, 'locales'), path.resolve('..', '..', 'locales'));
-console.info('Theme installed');
+if (process.cwd() != __dirname) {
+    console.info('Installing theme default');
+    fs.ensureDirSync(path.resolve(__dirname, '..', '..', '.uibuild'));
+    fs.copySync(path.resolve(__dirname, 'templates'), path.resolve(__dirname, '..', '..', '.uibuild', 'templates'));
+    fs.copySync(path.resolve(__dirname, 'public'), path.resolve(__dirname, '..', '..', '.uibuild', 'public'));
+    fs.copySync(path.resolve(__dirname, 'locales'), path.resolve(__dirname, '..', '..', '.uibuild', 'locales'));
+    console.info('Theme installed');
+} else console.log('Theme install ignored');
